36e030b37a9b698b62a2c8dfb0580cb26e78e950
ali aziz <aa_connect@outlook.com>
2018-07-06 12:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten(name) {
-  return kittens.slice(0, 3)
+  return kittens.slice(0, kittens.length-3)
 }
 function removeFirstKitten(name) {
   return kittens.slice(1)
